2200fc033bf30c234f7fae29a2e0a6ca5d96d1a5
武清伯 <wayneHu2014@163.com>
2021-06-21 00:47:33 +0800

    vue3组件与vue2组件的对比分析

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- vue2中的html模版必须要有一对根标签，vue3组件的html模版中可以没有根标签 -->
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- 使用子级组件 -->
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+// lang="ts" 表示这里可以使用ts代码
 
+// defineComponent函数：目的是定义一个组件，内部可以传入一个配置对象
+import { defineComponent } from "vue";
+// 引入一个子级组件
+import HelloWorld from "./components/HelloWorld.vue";
+
+// 暴露出去一个定义好的组件
 export default defineComponent({
-  name: 'App',
+  // 当前组件的名字是App
+  name: "App",
+  // 注册组件
   components: {
-    HelloWorld
-  }
+    // 注册一个子级组件
+    HelloWorld,
+  },
 });
 </script>
 
