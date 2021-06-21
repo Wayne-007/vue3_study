56f04e9905e0822b06c6f63f38700a5909c1703c
武清伯 <waynehu2014@163.com>
2021-06-21 22:05:13 +0800

    简化

--- 01_源码/App.vue
+++ 01_源码/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>base</div>
+  <!-- vue2中的html模版必须要有一对根标签，vue3组件的html模版中可以没有根标签 -->
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- 使用子级组件 -->
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
@@ -7,11 +10,18 @@
 
 // defineComponent函数：目的是定义一个组件，内部可以传入一个配置对象
 import { defineComponent } from "vue";
+// 引入一个子级组件
+import HelloWorld from "./components/HelloWorld.vue";
 
 // 暴露出去一个定义好的组件
 export default defineComponent({
   // 当前组件的名字是App
   name: "App",
+  // 注册组件
+  components: {
+    // 注册一个子级组件
+    HelloWorld,
+  },
 });
 </script>
 
