bf027551fd4fcebcc07790d772e6c3ecda2d642d
武清伯 <waynehu2014@163.com>
2021-06-29 23:37:27 +0800

    shallowReactive和shaloowRef

---
+++ 12_ref获取页面元素/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>ref的另一个作用：可以获取页面中的元素</h2>
+  <input type="text" ref="inputRef" />
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+
+export default defineComponent({
+  name: "App",
+
+  // 需求：当页面加载完毕后，页面中的文本框可以自动获取焦点
+  setup() {
+    const inputRef = ref<HTMLElement | null>(null);
+
+    onMounted(() => {
+      inputRef.value && inputRef.value.focus();
+    });
+    return {
+      inputRef,
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
