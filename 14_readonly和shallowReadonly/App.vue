29dfe76cd4a0412c902c2524688e3ae14c62e52e
武清伯 <waynehu2014@163.com>
2021-06-30 23:23:05 +0800

    readonly和shallowReadonly

---
+++ 14_readonly和shallowReadonly/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h2>readonly和shallowReadonly</h2>
+  <p>state：{{ state }}</p>
+  <p>state2：{{ state2 }}</p>
+  <p>state3：{{ state3 }}</p>
+  <button @click="update">修改数据</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, readonly, shallowReadonly } from "vue";
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      name: "佐助",
+      car: {
+        name: "宝马",
+      },
+    });
+    const state2 = readonly(state);
+    const state3 = shallowReadonly(state);
+    const update = () => {
+      // state.name += "==>";
+      // 报错：深度只可读
+      // state2.name += "==>";
+      // 浅只可读，且修改数据会影响到前面的数据
+      // state3.name += "==>";
+      state3.car.name += "==>";
+    };
+    return {
+      state,
+      state2,
+      state3,
+      update,
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
