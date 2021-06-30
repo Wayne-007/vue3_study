22b23a92857972ed7248a8f6df83426f8c096355
武清伯 <waynehu2014@163.com>
2021-06-30 23:50:46 +0800

    对于toRaw和markRaw的补充

--- 15_toRaw和markRaw/App.vue
+++ 15_toRaw和markRaw/App.vue
@@ -7,6 +7,18 @@
 </template>
 
 <script lang="ts">
+/* toRaw：
+1、返回由reactive或readonly方法转换成响应式代理的普通对象
+2、这是一个还原方法，可用于临时读取，访问不会被代理/跟踪，写入时也不会触发界面更新
+ */
+
+/* markRaw：
+1、标记一个对象，使其永远不会转换为代理，返回对象本身；
+2、应用场景：
+  * 有些值不应被设置为响应式的，例如：复杂的第三方类实例或Vue组件对象；
+  * 当渲染具有不可变数据源的大列表时，跳过代理转换可以提高性能；
+ */
+
 import { defineComponent, markRaw, reactive, toRaw } from "vue";
 interface IUserInfo {
   name: string;
