7c73f6c7e3f24956884954fd2a48a24aae4f1120
武清伯 <waynehu2014@163.com>
2021-07-04 00:59:03 +0800

    响应式数据的判断

---
+++ 18_响应式数据的判断/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h2>响应式数据的判断</h2>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  isProxy,
+  isReactive,
+  isReadonly,
+  isRef,
+  reactive,
+  readonly,
+  ref,
+} from "vue";
+
+export default defineComponent({
+  name: "App",
+
+  /* 
+  isRef：检查一个值是否为一个ref对象
+  isReactive：检查一个对象是否由reactive创建的响应式代理
+  isReadonly：检查一个对象是否是由readonly创建的只读代理
+  isProxy：检查一个对象是否是由reactive或者readonly方法创建的代理
+ */
+
+  setup() {
+    console.log(isRef(ref({})));
+    console.log(isReactive(reactive({})));
+    console.log(isReadonly(readonly({})));
+    console.log(isProxy(readonly({})));
+    console.log(isProxy(reactive({})));
+
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <h2>provide和inject</h2>
-  <p>当前的颜色：{{ color }}</p>
-  <button @click="color = 'red'">红色</button>
-  <button @click="color = 'yellow'">黄色</button>
-  <button @click="color = 'green'">绿色</button>
-  <hr />
-  <Son />
+  <div></div>
 </template>
 
 <script lang="ts">
-import { defineComponent, provide, ref } from "vue";
-import Son from "./components/Son.vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "App",
-  components: {
-    Son,
-  },
   setup() {
-    const color = ref("red");
-    provide("color", color);
-    return { color };
+    return {};
   },
 });
 </script>
