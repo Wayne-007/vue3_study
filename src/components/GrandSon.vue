8b965b498fb303ebe5a0df259659087916693828
武清伯 <waynehu2014@163.com>
2021-07-04 00:51:14 +0800

    provide和inject

---
+++ src/components/GrandSon.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2 :style="`color:${color}`">GrandSon孙子组件</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from "vue";
+
+export default defineComponent({
+  name: "GrandSon",
+  setup() {
+    const color = inject("color");
+    return { color };
+  },
+});
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Son.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>Son子组件</h2>
+  <hr />
+  <GrandSon />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import GrandSon from "./GrandSon.vue";
+
+export default defineComponent({
+  name: "Son",
+  components: {
+    GrandSon,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
