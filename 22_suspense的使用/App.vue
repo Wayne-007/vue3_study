cad73899b92e81d8725afbc77a048c91478582bd
武清伯 <waynehu2014@163.com>
2021-07-04 22:43:52 +0800

    suspens的使用

---
+++ 22_suspense的使用/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h2>
+    Suspense（悬疑）：允许我们的应用程序在等待移步组件时渲染一些后备内容，可以让我们创建一个平滑的用户体验
+  </h2>
+  <Suspense>
+    <template #default>
+      <!-- 异步组件 -->
+      <!-- <AsyncComponent /> -->
+      <AsyncAddress />
+    </template>
+    <template v-slot:fallback>
+      <!-- loading的内容 -->
+      <h2>Loading</h2>
+    </template>
+  </Suspense>
+</template>
+
+<script lang="ts">
+import { defineComponent, defineAsyncComponent } from "vue";
+
+// 组件的引入有：动态引入和静态引入
+
+// vue2中的动态引入方式：（在vue3中这种写法不行）
+// const AsyncComponent = () => import("./components/AsyncComponent.vue");
+
+// vue3中的动态引入组件的写法
+// const AsyncComponent = defineAsyncComponent(
+//   () => import("./components/AsyncComponent.vue")
+// );
+
+// import AsyncComponent from "./components/AsyncComponent.vue";
+
+import AsyncAddress from "./components/AsyncAddress.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    // AsyncComponent,
+    AsyncAddress,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
---
+++ 22_suspense的使用/components/AsyncAddress.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2>AsyncAddress</h2>
+  <p>{{ data }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  name: "AsyncAddress",
+  //   setup() {
+  //     return axios.get("/public/data/address.json").then((res) => {
+  //       console.log(res);
+  //       return {
+  //         data: res.data,
+  //       };
+  //     });
+  //     // return {}
+  //   },
+
+  async setup() {
+    const result = await axios.get("/public/data/address.json");
+    return {
+      data: result.data,
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
