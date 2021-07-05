0b193ecd3030a2b7ce7e8c69580998a30842f083
武清伯 <waynehu2014@163.com>
2021-07-05 20:06:28 +0800

    临时保存

---
+++ src/components/Item.vue
@@ -0,0 +1,56 @@
+<template>
+  <li>
+    <label>
+      <input type="checkbox" />
+      <span>xxxx</span>
+    </label>
+    <button class="btn btn-danger" style="display: none">删除</button>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Item",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: left;
+  display: none;
+  margin-top: 3px;
+}
+
+li::before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="todo-main">
+    <Item />
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Item from "./Item.vue";
+
+export default defineComponent({
+  components: {
+    Item,
+  },
+  name: "List",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.todo-main {
+  margin-left: 0;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
